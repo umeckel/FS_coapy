7ede148eea710efbbc83e01d93dbe314aa9a7b34
ulrich <umser@server>
2013-01-28 18:17:38 +0100

    Quickfix for unordered set in options.decode()

--- coapy/options.py
+++ coapy/options.py
@@ -36,8 +36,68 @@ import coapy.constants
 import types
 import struct
 import binascii
+import collections
 
 OPTION_TYPE_FENCEPOST = 14
+# TODO: QUICKFIX for unordered set in option.decode()
+# http://stackoverflow.com/questions/1653970/does-python-have-an-ordered-set
+# http://code.activestate.com/recipes/576694/
+class OrderedSet(collections.MutableSet):
+
+    def __init__(self, iterable=None):
+        self.end = end = [] 
+        end += [None, end, end]         # sentinel node for doubly linked list
+        self.map = {}                   # key --> [key, prev, next]
+        if iterable is not None:
+            self |= iterable
+
+    def __len__(self):
+        return len(self.map)
+
+    def __contains__(self, key):
+        return key in self.map
+
+    def add(self, key):
+        if key not in self.map:
+            end = self.end
+            curr = end[1]
+            curr[2] = end[1] = self.map[key] = [key, curr, end]
+
+    def discard(self, key):
+        if key in self.map:        
+            key, prev, next = self.map.pop(key)
+            prev[2] = next
+            next[1] = prev
+
+    def __iter__(self):
+        end = self.end
+        curr = end[2]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[2]
+    def __reversed__(self):
+        end = self.end
+        curr = end[1]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[1]
+
+    def pop(self, last=True):
+        if not self:
+            raise KeyError('set is empty')
+        key = self.end[1][0] if last else self.end[2][0]
+        self.discard(key)
+        return key
+
+    def __repr__(self):
+        if not self:
+            return '%s()' % (self.__class__.__name__,)
+        return '%s(%r)' % (self.__class__.__name__, list(self))
+
+    def __eq__(self, other):
+        if isinstance(other, OrderedSet):
+            return len(self) == len(other) and list(self) == list(other)
+        return set(self) == set(other)
 
 def length_of_vlint (value):
     """
@@ -714,7 +774,7 @@ def decode (num_options, payload):
     """
 
     type_val = 0
-    options = set()
+    options = OrderedSet()
     while 0 < num_options:
         num_options -= 1
         value_start_index = 1
